6eb6acd48ce62a1b898682ed2c24bbd3cd95459b
Kynosaur <kye.bracey@gmail.com>
2017-07-13 18:32:15 +0100

    Removes nested if statement from updateQuality()

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -15,66 +15,18 @@ class Shop {
       const item = this.items[i]
       if (item.name === 'normal') {
         this.normalUpdateQuality(item);
-        return this.items;
       }
       if (item.name === 'Aged Brie') {
         this.brieUpdateQuality(item);
-        return this.items;
       }
       if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
         this.backstageUpdateQuality(item);
-        return this.items;
       }
-
-
-
-
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
+      if (item.name === 'Sulfuras, Hand of Ragnaros') {
+        this.sulfurasUpdateQuality(item);
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
-    }
-
     return this.items;
+    }
   }
 
   normalUpdateQuality(item) {
@@ -98,5 +50,9 @@ class Shop {
     item.quality += 1
     if (item.sellIn < 10) { item.quality += 1 }
     if (item.sellIn < 5) { item.quality += 1 }
+  }
+
+  sulfurasUpdateQuality(item) {
+
   }
 }
